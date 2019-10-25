7a38a1f7caa6e8fb1c36d64f20e9dbfe387ae560
marco <devirgilis.marco@gmail.com>
2019-10-24 20:35:05 -0500

    evaluation functions

---
+++ final/Models/GBM/GBM.R
@@ -0,0 +1,131 @@
+rm(list = ls())
+
+library(tidyverse)
+library(xgboost)
+library(Matrix)
+library(magrittr)
+library(caret)
+library(pracma)
+
+setwd("/home/marco/Documents/gitreps/pricing/final")
+
+source("./Utils/utils.R")
+
+# Import Data -------------------------------------------------------------
+
+train <- import_data("./Data/train_final.csv") %>% mutate(id = "train")
+
+test <- import_data("./Data/test_final.csv") %>% mutate(id = "test")
+
+comb_data <- bind_rows(train, test)
+
+xgb_model_numb <- xgb.load("./Models/GBM/xgb_numbers")
+
+xgb_model_sev <- xgb.load("./Models/GBM/xgb_severity")
+
+
+# Add Variables -----------------------------------------------------------
+
+train_data_numb <- create_data_numb(filter(comb_data, id == "train") %>% select(-id))
+
+train_data_sev <- create_data_sev(filter(comb_data, id == "train", sev > 0) %>% select(-id))
+
+test_data_numb <- create_data_numb(filter(comb_data, id == "test") %>% select(-id))
+ 
+test_data_sev <- create_data_sev(filter(comb_data, id == "test") %>% select(-id))
+
+train_numb <- xgb.DMatrix(data = train_data_numb, label = filter(comb_data, id == "train") %>% pull(ClaimInd))
+
+train_sev <- xgb.DMatrix(data = train_data_sev, label = filter(comb_data, id == "train", sev > 0) %>% pull(sev))
+
+test_numb <- xgb.DMatrix(data = test_data_numb, label = filter(comb_data, id == "test") %>% pull(ClaimInd))
+
+test_sev <- xgb.DMatrix(data = test_data_sev, label = filter(comb_data, id == "test") %>% pull(sev))
+
+
+# Model Section -----------------------------------------------------------
+
+# Numbers
+
+# DON'T RUN
+# 
+# params <- list(booster = "gbtree", 
+#                objective = "count:poisson",
+#                eval_metric = "poisson-nloglik",
+#                eta = 0.1, 
+#                gamma = 1, 
+#                max_depth = 6)
+# 
+# 
+# xgbcv <- xgb.cv(params = params, 
+#                 data = train_numb, 
+#                 nrounds = 1000, 
+#                 nfold = 5, 
+#                 showsd = TRUE, 
+#                 stratified = TRUE, 
+#                 print_every_n = 100, 
+#                 early_stop_round = 20, 
+#                 maximize = FALSE,
+#                 prediction = TRUE)
+# 
+# xgb_model_numb <- xgb.train(data = train_numb,
+#                             params = params,
+#                             watchlist = list(train = train_numb, test = test_numb),
+#                             nrounds = which.min(xgbcv$evaluation_log$train_poisson_nloglik_mean),
+#                             verbose = 1,
+#                             print_every_n = 10)
+
+numb_pred <- predict(xgb_model_numb, test_numb)
+
+# Severity
+
+# DON'T RUN
+# 
+# params <- list(booster = "gbtree", 
+#                eval_metric = "gamma-nloglik",
+#                objective = "reg:gamma",
+#                eta = .01, 
+#                gamma = 1, 
+#                max_depth = 10)
+# 
+# 
+# xgbcv <- xgb.cv(params = params, 
+#                 data = train_sev, 
+#                 weight = train$ClaimInd,
+#                 nrounds = 10000, 
+#                 nfold = 5, 
+#                 showsd = TRUE, 
+#                 stratified = TRUE, 
+#                 print_every_n = 100, 
+#                 early_stop_round = 20, 
+#                 maximize = FALSE,
+#                 prediction = TRUE)
+# 
+# xgb_model_sev <- xgb.train(data = train_sev,
+#                            weight = train$ClaimInd,
+#                            params = params,
+#                            watchlist = list(train = train_sev, test = test_sev),
+#                            nrounds = which.min(xgbcv$evaluation_log$train_gamma_nloglik_mean),
+#                            verbose = 1,
+#                            print_every_n = 100)
+
+sev_pred <- predict(xgb_model_sev, test_sev)
+
+# Prediction
+
+test <- test %>% mutate(observed_lc = ClaimAmount / exposure,
+                        predicted_lc = numb_pred * sev_pred / exposure)
+
+
+# Performance Evaluation --------------------------------------------------
+
+eval_dataset <- test %>% select(exposure, observed_lc, predicted_lc)
+
+eval_dataset %$% NRMSE(predicted_lc, observed_lc)
+
+eval_dataset %$% gini_plot(predicted_lc, exposure)
+
+eval_dataset %$% gini_value(predicted_lc, exposure)
+  
+eval_dataset %$% lift_curve_plot(predicted_lc, observed_lc, 20) +
+                 scale_y_log10()
---
+++ final/Utils/utils.R
@@ -0,0 +1,121 @@
+
+# Import Data Functions ----------------------------------------------------
+
+import_data <- function(data){
+  
+  read_csv(data, 
+           col_types = cols(Gender = col_character(), 
+                            VehAge = col_character(), 
+                            HasKmLimit = col_factor())) %>% 
+    mutate_if(is.character, as.factor) %>% 
+    filter((ClaimInd==0 & ClaimAmount==0) | (ClaimInd>0 & ClaimAmount>0)) %>% 
+    mutate(sev = ifelse(ClaimInd == 0, 0, ClaimAmount / ClaimInd))
+  
+}
+
+create_data_numb <- function(data){
+  
+  sparse.model.matrix(ClaimInd ~
+                        LicAge +
+                        VehAge +
+                        Gender +
+                        MariStat +
+                        SocioCateg +
+                        VehUsage +
+                        DrivAge +
+                        HasKmLimit +
+                        BonusMalus +
+                        VehBody +
+                        VehPrice +
+                        VehEngine +
+                        VehEnergy      +
+                        VehMaxSpeed +
+                        VehClass +
+                        Garage +
+                        LicAge_Band +
+                        BonusMalus_Cat +
+                        DrivAge_Band +
+                        VehPriceGrp, data = data)}
+
+create_data_sev <- function(data){
+  
+  sparse.model.matrix(sev ~
+                        LicAge +
+                        VehAge +
+                        Gender +
+                        MariStat +
+                        SocioCateg +
+                        VehUsage +
+                        DrivAge +
+                        HasKmLimit +
+                        BonusMalus +
+                        VehBody +
+                        VehPrice +
+                        VehEngine +
+                        VehEnergy      +
+                        VehMaxSpeed +
+                        VehClass +
+                        Garage +
+                        LicAge_Band +
+                        BonusMalus_Cat +
+                        DrivAge_Band +
+                        VehPriceGrp, data = data)}
+
+NRMSE <- function(pred, obs){
+  
+  RMSE(pred, obs)/(max(obs)-min(obs))
+  
+}
+
+gini_plot <- function(predicted_loss_cost, exposure){
+  
+  lc_var <- enquo(predicted_loss_cost)
+  exp <- enquo(exposure)
+  
+  dataset <- tibble(lc_var = !! lc_var, exp = !! exp)
+  
+  dataset %>% 
+    arrange(lc_var) %>% 
+    mutate(cum_exp = cumsum(exp) / sum(exp),
+           cum_pred_lc = cumsum(lc_var) / sum(lc_var)) %>% 
+    ggplot()+
+    geom_line(aes(x = cum_exp, y = cum_pred_lc))+
+    geom_abline(intercept = 0, slope = 1)+
+    xlab("Exposure")+
+    ylab("Predicted Loss Cost")
+  
+}
+
+gini_value <- function(predicted_loss_cost, exposure){
+  
+  lc_var <- enquo(predicted_loss_cost)
+  exp <- enquo(exposure)
+  
+  dataset <- tibble(lc_var = !! lc_var, exp = !! exp)
+  
+  dataset %>% 
+    arrange(lc_var) %>% 
+    mutate(cum_exp = cumsum(exp) / sum(exp),
+           cum_pred_lc = cumsum(lc_var) / sum(lc_var)) %$% 
+    {trapz(cum_exp, cum_pred_lc) %>% add(-1) %>% abs() %>% subtract(.5) %>% multiply_by(2)}
+}
+
+lift_curve_plot <- function(predicted_loss_cost, observed_loss_cost, n){
+  
+  pred_lc <- enquo(predicted_loss_cost)
+  obs_lc <- enquo(observed_loss_cost)
+  
+  dataset <- tibble(pred_lc = !! pred_lc, obs_lc = !! obs_lc)
+
+  dataset %>% 
+    arrange(pred_lc) %>% 
+    mutate(buckets = ntile(pred_lc, n)) %>% 
+    group_by(buckets) %>% 
+    summarise(avg_pred = mean(pred_lc),
+              avg_obs = mean(obs_lc))%>% 
+    pivot_longer(c(avg_pred,avg_obs)) %>%
+    ggplot() +
+    geom_line(aes(x = as.factor(buckets), y = value, col = name, group = name))+
+    geom_point(aes(x = as.factor(buckets), y = value, col = name, group = name))+
+    xlab("Bucket")
+}
