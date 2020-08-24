00b5a5b91b71894aa43d25b48167b38db73481b0
Peter Dyson <peter@dysonmail.me>
2020-08-24 19:39:01 +0100

    Updated model to remove pre-processing which is now done centrally.

--- PD/version 2/data_cleansing.py
+++ PD/version 2/data_cleansing.py
@@ -2,93 +2,15 @@
 
 import pandas as pd
 
-def load_file(data_filename):
-    return pd.read_csv(data_filename)
+def load_file():
+    data_train_file = r'../../final/Data/train_new_final.csv'
+    data_train = pd.read_csv(data_train_file).set_index('RecordID')
     
-
-
-def clean_data_strict(data_raw):
+    data_test_file = r'../../final/Data/test_new_final.csv'
+    data_test = pd.read_csv(data_test_file).set_index('RecordID')
     
-    #create dataframe to adjust
-    data = data_raw.copy()
-
-    #
-    # Process Target Vars
-    #
-
-    # ClaimNb_binary
-    # create binary claim flag to use as categorical variable (remove multi claim policies)
-    data['ClaimNb_Binary'] = 0
-    data['ClaimNb_Binary'] = data['ClaimNb_Binary'].where(data['ClaimNb']==0,1)
+    return data_train, data_test
     
-    # Freq (claims per year)
-    # adjust to definition from adjusted claim number
-    data['Freq'] = data['ClaimNb_Binary']/data['Exposure']
-    
-    
-    #
-    # Process Factors
-    #
-    
-    # Exposure
-    # filter on exposure of more than 10% of year, and remove exposures>1year
-    data = data.loc[(data['Exposure']>= 0.1) & (data['Exposure']<=1)]
-    
-    # Area
-    # convert to category
-    data['Area'] = data['Area'].astype('category')
-    
-    #VehPower
-    map_vehpower = {1:'L',
-                    2:'L',
-                    3:'L',
-                    4:'L',
-                    5:'L',
-                    6:'M',
-                    7:'M',
-                    8:'M',
-                    9:'M',
-                    10:'M',
-                    11:'H',
-                    12:'H',
-                    13:'H',
-                    14:'H',
-                    15:'H',
-                    }
-    data['VehPower'].replace(map_vehpower, inplace=True)
-
-    # BonusMalus
-    bins = [49, 60, 80, 100, 200, 350]
-    data['BonusMalus'] = pd.cut(data['BonusMalus'], bins)
-    
-    # VehBrand
-    #nothing
-
-    # VehGas
-    #nothing
-
-    # Region
-    #nothing
-
-    # DrivAgeBand
-    # drop young drivers as there are very few observations
-    data = data.loc[data['DrivAgeBand']!='18, 25']
-
-    
-    # DensityBand
-    #nothing
-
-    # VehAgeBand
-    #nothing
-
-    category_list = ['ClaimNb_Binary','VehPower','BonusMalus','DrivAgeBand','DensityBand','VehAgeBand','Region','VehGas','VehBrand']
-    for cat in category_list:
-        data[cat] = data[cat].astype('category')
-
-    
-    return data
-
-
 def clean_data_minimal(data_raw):
     
     #create dataframe to adjust
@@ -98,49 +20,37 @@ def clean_data_minimal(data_raw):
     # Process Target Vars
     #
 
-    # ClaimNb_binary
-    # create binary claim flag to use as categorical variable (remove multi claim policies)
-    data['ClaimNb_Binary'] = 0
-    data['ClaimNb_Binary'] = data['ClaimNb_Binary'].where(data['ClaimNb']==0,1)
-    
-    # Freq (claims per year)
-    # adjust to definition from adjusted claim number
-    data['Freq'] = data['ClaimNb_Binary']/data['Exposure']
-    
+    #nothing
 
     #
     # Apply filters to the data
     #
 
     # Exposure
-    # filter on exposure of more than 10% of year, and remove exposures>1year
-    data = data.loc[(data['Exposure']>= 0.1) & (data['Exposure']<=1)]
+    #nothing
 
-    # drop young drivers as there are very few observations and this is a high risk category
-    data = data.loc[data['DrivAgeBand']!='18, 25']
     
     #
     # Process Factors
     #
     
     # Area
-    # convert to category
     #nothing
     
-    #VehPower
+    # VehPower
     #nothing
 
     # BonusMalus
     #nothing
     
     # VehBrand
-    data['VehBrand'] = data['VehBrand'].apply(lambda x: x.replace('B', '')).astype(int)
+    #nothing
 
     # VehGas
     #nothing
 
     # Region
-    data['Region'] = data['Region'].apply(lambda x: x.replace('R', '')).astype(int)
+    #nothing
 
     # DrivAgeBand
     #nothing
@@ -149,11 +59,8 @@ def clean_data_minimal(data_raw):
     #nothing
 
     # VehAgeBand
-    data['VehAgeBand'] = data['VehAgeBand'].apply(lambda x: x.replace('+', '')).astype(int)
+    #nothing
 
-    category_list = ['ClaimNb_Binary','VehPower','BonusMalus','DrivAgeBand','DensityBand','VehAgeBand','Region','VehGas','VehBrand']
-    for cat in category_list:
-        data[cat] = data[cat].astype('category')
     
     return data
 
@@ -162,11 +69,10 @@ def clean_data_minimal(data_raw):
 if __name__ =='__main__':
     
     
-    data_train_file = r'../final/Data/train_new_final.csv'
-    data_train_raw = load_file(data_train_file)
+    data_train_raw, data_test_raw = load_file()
     
-    data_test_file = r'../final/Data/test_new_final.csv'
-    data_test_raw = load_file(data_test_file)
+    data_test = data_test_raw.copy()
+    data_train = data_train_raw.copy()
     
     # apply cleaning steps [:limit rows]
     data_train = clean_data_minimal(data_train_raw)
--- PD/version 2/modelling_freq_regressor.py
+++ PD/version 2/modelling_freq_regressor.py
@@ -19,94 +19,102 @@ import data_utils as du
 # load the and process data
 #
 
-data_train_file = r'../final/Data/train_new_final.csv'
-data_train_raw = dc.load_file(data_train_file)
+data_train_raw, data_test_raw = dc.load_file()
 
-data_test_file = r'../final/Data/test_new_final.csv'
-data_test_raw = dc.load_file(data_test_file)
+data_test = data_test_raw.copy()
+data_train = data_train_raw.copy()
 
-# apply cleaning steps [:limit rows]
-data_train = dc.clean_data_minimal(data_train_raw)
-data_test = dc.clean_data_minimal(data_test_raw)
+
+#%%########################################################################
+#
+# Frequency Projection
+#
+
+#%% set target
+data_train['freq'] = data_train['ClaimNb']/data_train['Exposure']
+data_test['freq'] = data_test['ClaimNb']/data_test['Exposure']
+target_name='freq'
 
 #drop alternative targets
-drop_list = ['ClaimNb', 'Exposure', 'ClaimAmount','ClaimNb_Binary']
+drop_list = ['ClaimNb', 'Exposure', 'ClaimAmount', 'BonusMalus', 'severity']
 data_train.drop(drop_list,axis='columns', inplace=True)
 data_test.drop(drop_list,axis='columns', inplace=True)
 
-#oversample the claim ovservations to create a balanced data set
-target_name='Freq'
-data_train_resampled = du.oversample_training_set(data_train, target_name)
+#%% lable encode factors
+label_encode_factors = ['Area',
+                        'VehPower',
+                        'VehBrand',
+                        'VehGas',
+                        #'Density',
+                        'Region',
+                        'DrivAgeBand',
+                        'DensityBand',
+                        'VehAgeBand']
+data_train_encoded, encoders = du.preprocess_labelencode(data_train, label_encode_factors)
+data_test_encoded = du.preprocess_labelencode_apply(encoders, data_test, label_encode_factors)
 
-#create head to reivew
-data_train_res_head = data_train_resampled.head(100)
-data_train_res_head.dtypes
+#%% oversample the claim ovservations to create a balanced data set
+data_train_resampled = du.oversample_training_set(data_train_encoded, target_name)
+
+#%% split factors and target
+
+#sets used to train
+X_train = data_train_resampled.drop(target_name, axis='columns')
+y_train = data_train_resampled[target_name]
+
+#sets used to predict
+X_train_to_predict = data_train_encoded.drop(target_name, axis='columns')
+X_test = data_test_encoded.drop(target_name, axis='columns')
+
 
 #%%
 #
-# preprocessing and encoding
-#
-
-
-label_encode_factors = ['Area', 'VehGas','DrivAgeBand','DensityBand']
-
-data_train_encoded, encoders = du.preprocess_labelencode(data_train_resampled, label_encode_factors)
-data_test_encoded = du.preprocess_labelencode_apply(encoders, data_test, label_encode_factors)
-
-X_train = data_train_encoded.drop(target_name, axis='columns')
-y_train = data_train_encoded[target_name]
-
-X_test = data_test_encoded.drop(target_name, axis='columns')
-y_test = data_test_encoded[target_name]
-
-
-#%%########################################################################
-#
 # fit a random forest
 #
+if False:
+    clf_rf = RandomForestRegressor(min_samples_leaf=5, 
+                                    n_estimators=100,
+                                    max_depth=50,
+                                    max_features='auto',
+                                    criterion='mse',    #mse or mae
+                                    verbose=True,
+                                    n_jobs=-1)
+    clf_rf = clf_rf.fit(X_train.values, y_train.values)    
 
-clf_rf = RandomForestRegressor(min_samples_leaf=10, 
-                                n_estimators=500,
-                                max_depth=15,
-                                criterion='mse',    #mse or mae
-                                verbose=True,
-                                n_jobs=-1)
-clf_rf = clf_rf.fit(X_train.values, y_train.values)    
-
-y_train_predicted_rf = clf_rf.predict(X_train.values)    
+#%% predict values
+y_train_predicted_rf = clf_rf.predict(X_train_to_predict.values)    
 y_test_predicted_rf = clf_rf.predict(X_test.values)   
 
-y_train_predicted_rf_prob = clf_rf.predict_proba(X_train.values)    
-y_test_predicted_rf_prob = clf_rf.predict_proba(X_test.values)   
+#%% add results back to dataset
+data_train_raw['freq_predicted'] = y_train_predicted_rf
+data_train_raw['ClaimNb_predicted'] = y_train_predicted_rf*data_train_raw['Exposure']
+data_train_raw['ClaimNb_predicted'] = data_train_raw['ClaimNb_predicted']>0.55
 
-y_zero_test = [0]*len(y_test)
+data_test_raw['freq_predicted'] = y_test_predicted_rf
+data_test_raw['ClaimNb_predicted'] = y_test_predicted_rf*data_test_raw['Exposure']
+data_test_raw['ClaimNb_predicted'] = data_test_raw['ClaimNb_predicted']>0.55
+
 
 #%% print resutls
-print('Train Original :', len(y_train), np.mean(list(y_train.values)))
-print('Train Predicted:',len(y_train_predicted_rf), np.mean(y_train_predicted_rf))
-print('Test Original  :', len(y_test), np.mean(list(y_test.values)))
-print('Test Predicted :',len(y_test_predicted_rf), np.mean(y_test_predicted_rf))
+print('Train Original :', len(data_train_raw), data_train_raw['ClaimNb'].sum())
+print('Train Predicted:', len(data_train_raw), data_train_raw['ClaimNb_predicted'].sum())
 
-print('Train Gini     :', du.gini(y_train, y_train_predicted_rf))
-print('Test Gini      :', du.gini(y_test, y_test_predicted_rf))
-print('Zeros Gini     :', du.gini(y_test, y_zero_test))
+print('Train Original :', len(data_test_raw), data_test_raw['ClaimNb'].sum())
+print('Train Predicted:', len(data_test_raw), data_test_raw['ClaimNb_predicted'].sum())
 
-df_features = pd.DataFrame(clf_rf.feature_importances_, index=X_train.columns)
+print('Train Gini     :', du.gini(data_train_raw['ClaimNb'], data_train_raw['ClaimNb_predicted']))
+print('Test Gini      :', du.gini(data_test_raw['ClaimNb'], data_test_raw['ClaimNb_predicted']))
+
+#df_features = pd.DataFrame(clf_rf.feature_importances_, index=X_train.columns)
 
 
 #%%########################################################################
 #
-# Calc lift curve (gains chart)
+# Output to file
 #
 
-import scikitplot as skplt
-
-skplt.metrics.plot_cumulative_gain(y_test, y_train_predicted_rf)
-skplt.metrics.plot_cumulative_gain(y_train, y_test_predicted_rf)
-
-
-
-
+data_train_raw.to_csv(r'data_train_output.csv')
+data_test_raw.to_csv(r'data_test_output.csv')
 
 
 
