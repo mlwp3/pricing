6c8c952c40584384908a15451012675283a7dc1b
Daniel <danlupton@gmail.com>
2020-09-11 09:37:06 -0500

    Update for new data. Doesn't seem to work, though.

--- final/Models/MARS/Comparison MARS 12 4 2019.R
+++ final/Models/MARS/Comparison MARS 12 4 2019.R
@@ -3,50 +3,51 @@ library(tidyverse)
 
 source("final/Utils/utils.R")
 
-train <- import_data("final/Data/train_final.csv") %>% mutate(id = "train")
+train <- import_data("final/Data/train_new_final.csv") %>% mutate(id = "train")
 
-test <- import_data("final/Data/test_final.csv") %>% mutate(id = "test")
+test <- import_data("final/Data/test_new_final.csv") %>% mutate(id = "test")
 
-train$Freq <- train$ClaimInd / train$exposure
-train$PurePrem <- train$ClaimAmount / train$exposure
+train$Freq <- train$ClaimNb / train$Exposure
+train$PurePrem <- train$ClaimAmount / train$Exposure
 
-test$Freq <- test$ClaimInd / test$exposure
-test$PurePrem <- test$ClaimAmount / test$exposure
+test$Freq <- test$ClaimNb / test$Exposure
+test$PurePrem <- test$ClaimAmount / test$Exposure
 
 #Remove some variables from the training set.
 #This is because the earth package automatically considers every possible variable, and I don't want it
 #to try to predict the claim frequency based on the claim indicator, for example. That would be cheating!
 freq_train <- train
-freq_train$exposure <- NULL
-freq_train$ClaimInd <- NULL
+freq_train$Exposure <- NULL
+freq_train$ClaimNb <- NULL
 freq_train$ClaimAmount <- NULL
 freq_train$PurePrem <- NULL
-freq_train$X <- NULL
+freq_train$RecordID <- NULL
 
 freq_mod <- freq_train %>% 
   select(-id) %>% 
   earth(Freq ~ ., data = ., degree = 3)
 
+
 #The severity model is based on only those claims for which the claim amount is nonzero.
 sev_train <- train[which(train$ClaimAmount > 0),]
 
 sev_train$PurePrem <- NULL
-sev_train$X <- NULL
+sev_train$RecordID <- NULL
 sev_train$Freq <- NULL
-sev_train$exposure <- NULL
+sev_train$Exposure <- NULL
 sev_mod <- sev_train %>% 
   select(-id) %>% 
   earth(ClaimAmount ~ ., data = ., degree = 3)
 
-fs_pred <- predict(freq_mod, newdata = test)*predict(sev_mod, newdata = test) * test$exposure
-RMSE(fs_pred,test$ClaimAmount) #2238.138
-NRMSE(fs_pred,test$ClaimAmount) #0.01444359
-gini_value(as.numeric(fs_pred),test$exposure)
+fs_pred <- predict(freq_mod, newdata = test)*predict(sev_mod, newdata = test) * test$Exposure
+RMSE(fs_pred,test$ClaimAmount) #2069.684
+NRMSE(fs_pred,test$ClaimAmount) #0.005296801
+gini_value(as.numeric(fs_pred),test$Exposure) #-0.05571802
 
-gini_plot_fs <- gini_plot(as.numeric(fs_pred),test$exposure) + 
+gini_plot_fs <- gini_plot(as.numeric(fs_pred),test$Exposure) + 
   ggtitle("Frequency x Severity MARS Gini Plot")
 
-tbl_lift_fs <- lift_curve_table(as.numeric(fs_pred), test$ClaimAmount, test$exposure,10)
+tbl_lift_fs <- lift_curve_table(as.numeric(fs_pred), test$ClaimAmount, test$Exposure,10)
 plt_lift_fs <- tbl_lift_fs %>% 
   lift_curve_plot() +   
   labs(col = "Model")
@@ -55,20 +56,20 @@ plt_lift_fs
 train$ClaimAmount <- NULL
 train$Freq <- NULL
 train$ClaimInd <- NULL
-train$exposure <- NULL
+train$Exposure <- NULL
 pp_mod <- train %>% 
   select(-id) %>% 
   earth(PurePrem ~ ., data = ., degree = 3)
 
-pp_pred <- predict(pp_mod, newdata = test)*test$exposure
-RMSE(pp_pred,test$ClaimAmount) #2025.51
-NRMSE(pp_pred,test$ClaimAmount) #0.01307142
-gini_value(as.numeric(pp_pred),test$exposure)
+pp_pred <- predict(pp_mod, newdata = test)*test$Exposure
+RMSE(pp_pred,test$ClaimAmount) #2055.142
+NRMSE(pp_pred,test$ClaimAmount) #0.005259585
+gini_value(as.numeric(pp_pred),test$Exposure) #0.9236354
 
-gini_plot_pp <- gini_plot(as.numeric(pp_pred),test$exposure) + 
+gini_plot_pp <- gini_plot(as.numeric(pp_pred),test$Exposure) + 
   ggtitle("Pure Premium MARS Gini Plot")
 
-tbl_lift_pp <- lift_curve_table(as.numeric(pp_pred),test$ClaimAmount,test$exposure,10)
+tbl_lift_pp <- lift_curve_table(as.numeric(pp_pred),test$ClaimAmount,test$Exposure,10)
 
 plt_lift_pp <- tbl_lift_pp %>% 
   lift_curve_plot() + 
