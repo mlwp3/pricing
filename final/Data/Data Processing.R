a1c80e3a4b0a5f897a1bb050a4aa8760c18fc2a3
Marco De Virgilis <devirgilis.marco@gmail.com>
2020-10-08 18:50:19 -0500

    review data prep

--- final/Data/Data Processing.R
+++ final/Data/Data Processing.R
@@ -1,12 +1,18 @@
 library(dplyr)
 library(caret)
 library(readr)
+library(CASdatasets)
 
-load("~/Desktop/CAS MLWP/Pricing/freMTPL Data/freMTPL2freq.rda")
-freq <- freMTPL2freq
+rm(list = ls())
+
+data("freMTPL2freq")
+data("freMTPL2sev")
+
+# load("~/Desktop/CAS MLWP/Pricing/freMTPL Data/freMTPL2freq.rda")
+freq <- freMTPL2freq 
 rm(freMTPL2freq)
 
-sev <- load("~/Desktop/CAS MLWP/Pricing/freMTPL Data/freMTPL2sev.rda")
+# sev <- load("~/Desktop/CAS MLWP/Pricing/freMTPL Data/freMTPL2sev.rda")
 sev <- freMTPL2sev
 rm(freMTPL2sev)
 
@@ -23,14 +29,16 @@ colnames(dat)
 dat <- dat %>%
   dplyr::group_by(IDpol) %>%
   mutate(ClaimAmount = cumsum(ClaimAmount),
-         id = row_number(), max_id = max(id)) %>%
+         id = row_number(), 
+         max_id = max(id)) %>%
   filter(id == max_id) %>%
   select(-c(id, max_id)) %>%
   filter(!(ClaimNb > 0 & ClaimAmount == 0))
 
 dat <- dat %>%
   filter(Exposure > (1/12) & Exposure <= 1 & ClaimAmount < 1000000) %>%
-  mutate(RecordID = row_number(), severity = ifelse(ClaimNb == 0, 0, ClaimAmount / ClaimNb),
+  mutate(RecordID = row_number(), 
+         severity = ifelse(ClaimNb == 0, 0, ClaimAmount / ClaimNb),
          DrivAgeBand = as.character(cut(DrivAge, c(17, 25, 35, 45, 55, 65, Inf))),
          DensityBand = as.character(cut(Density, c(0, 50, 100, 200, 400, 1000, 2000, 5000, Inf))),
          VehAgeBand = ifelse(VehAge <= 15, as.character(VehAge), "15+"),
@@ -47,7 +55,10 @@ ind <- createDataPartition(dat$ClaimAmount, 0.7, times = 1, list = FALSE)
 train <- dat[ind, ]
 test <- dat[-ind, ]
 
-mean(train$ClaimAmount); mean(test$ClaimAmount); var(train$ClaimAmount); var(test$ClaimAmount)
+mean(train$ClaimAmount)
+mean(test$ClaimAmount)
+var(train$ClaimAmount)
+var(test$ClaimAmount)
 rm(ind)
 
 readr::write_csv(train, "~/Desktop/CAS MLWP/Pricing/freMTPL Data/train_new_final.csv")
