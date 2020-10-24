b056b98ed666d9a340c3569b29618b9e0c243fb5
Daniel <danlupton@gmail.com>
2020-10-24 12:39:21 -0500

    Add MARS Pure Premium model (WHICH DOESN'T DO ANYTHING) and revise my evaluation stats per Navarun

--- final/Models/MARS/Comparison MARS 9 15 2020.R
+++ final/Models/MARS/Comparison MARS 9 15 2020.R
@@ -13,7 +13,7 @@ source("final/Utils/perf_eval.R")
 set.seed(42)
 
 train <- import_data("final/Data/train.csv") %>% mutate(severity=ClaimAmount/ClaimNb)
-sev_train <- train %>% filter(ClaimAmount>0) #It's about the same if you choose claim amount > 0 or > 100
+sev_train <- train %>% filter(ClaimAmount>0) #It's about the same if you choose claim amount > 0 or > 100, but we'll stick with >0
 
 test <- import_data("final/Data/test.csv") %>% mutate(severity=ClaimAmount/ClaimNb)
 
@@ -55,35 +55,15 @@ summary(sev_mod)
 
 fs_pred <- predict(freq_mod, newdata = test, type="response")*predict(sev_mod, newdata = test, type="response")
 
-test <- test %>% mutate(observed_loss_cost = ClaimAmount / Exposure,
-                        predicted_loss_cost_fs = fs_pred / Exposure)
-FS_rmse <- rmse(test,"observed_loss_cost", test$predicted_loss_cost_fs)*100 #73.52
-FS_nrmse <- 1-NRMSE(test,"observed_loss_cost", test$predicted_loss_cost_fs)*100 #0.3684164
-FS_cor_pearson <- as.numeric(cor(test$predicted_loss_cost_fs,test$observed_loss_cost))
-FS_cor_spearman <- as.numeric(cor(test$predicted_loss_cost_fs,test$observed_loss_cost,method="spearman"))
-FS_mae <- MAE(test$predicted_loss_cost_fs, test$observed_loss_cost)
-FS_nmae <- 1-NMAE(test$predicted_loss_cost_fs, test$observed_loss_cost)*100
-FS_gini <- gini_value(test$predicted_loss_cost_fs,test$Exposure) #0.1756933
-FS_agg_rpr <- agg_rpr(test, "observed_loss_cost", test$predicted_loss_cost_fs)
-FS_norm_rpd <- norm_rp_deviance(test, "observed_loss_cost", test$predicted_loss_cost_fs)
-
-model_metrics <- data.frame("MARS Freq/Sev", FS_rmse, FS_mae, FS_cor_pearson, FS_cor_spearman, FS_gini, FS_agg_rpr)
-colnames(model_metrics) <- c("Model", "RMSE", "MAE", "Pearson (Linear) Correlation", "Spearman Rank Correlation", "Gini Index", "Aggregate Risk Premium Differential")
-eval_metrics <- data.frame(FS_gini, FS_nrmse, FS_nmae, FS_cor_spearman, FS_norm_rpd)
-lscore <- apply(eval_metrics, 1, mean)
-gscore <- apply(eval_metrics, 1, geometric_mean)
-hscore <- apply(eval_metrics, 1, harmonic_mean)
-
-exp(mean(log(c(FS_gini,FS_nrmse,FS_nmae,FS_cor_spearman,FS_norm_rpd))))
-mean(test$predicted_loss_cost_fs)
-mean(test$observed_loss_cost)
-
-model_metrics$lscore <- lscore
-model_metrics$gscore <- gscore
-model_metrics$hscore <- hscore
-model_metrics[is.na(model_metrics)] <- 0
-
-write.csv(model_metrics,"final/Output/MARS/MARS_model_evaluation_stats.csv")
+FS_rmse <- rmse(test,"ClaimAmount", fs_pred)*100
+FS_nrmse <- 1-NRMSE(test,"ClaimAmount", fs_pred)*100
+FS_cor_pearson <- as.numeric(cor(fs_pred,test$ClaimAmount))
+FS_cor_spearman <- as.numeric(cor(fs_pred,test$ClaimAmount,method="spearman"))
+FS_mae <- MAE(fs_pred, test$ClaimAmount)
+FS_nmae <- 1-NMAE(fs_pred, test$ClaimAmount)*100
+FS_gini <- gini_value(fs_pred,test$Exposure)
+FS_agg_rpr <- agg_rpr(test, "ClaimAmount", fs_pred)
+FS_norm_rpd <- norm_rp_deviance(test, "ClaimAmount", fs_pred)
 
 #Pure Premium MARS model basically doesn't work at all. Not yet at least?
 #It picks up no predictors at all.
@@ -103,47 +83,62 @@ write.csv(model_metrics,"final/Output/MARS/MARS_model_evaluation_stats.csv")
 # p_tuning$p.max
 # rm(p_values)
 # 
-# p <- 1.555102
+p <- 1.555102
+
+pp_mod <- earth(ClaimAmount ~ Area +
+                  VehPower +
+                  VehBrand +
+                  VehGas +
+                  Region +
+                  DrivAgeBand +
+                  DensityBand +
+                  VehAgeBand + offset(log(Exposure)), data = train, degree = 2, glm=list(family = tweedie(var.power = p, link.power = 0)))
+summary(pp_mod)
 # 
-# pp_mod <- earth(pmin(ClaimAmount,5000) ~ Area +
-#                   VehPower +
-#                   VehBrand +
-#                   VehGas +
-#                   Region +
-#                   DrivAgeBand +
-#                   DensityBand + 
-#                   VehAgeBand + offset(log(Exposure)), data = train, degree = 2, glm=list(family = tweedie(var.power = p, link.power = 0)))
-# summary(pp_mod)
-# 
-# pp_mod2 <- earth(pmin(round(ClaimAmount,0),5000) ~ Area +
-#                    VehPower +
-#                    VehBrand +
-#                    VehGas +
-#                    Region +
-#                    DrivAgeBand +
-#                    DensityBand + 
-#                    VehAgeBand + offset(log(Exposure)), data = train, degree = 2, glm=list(family = poisson(link="log")))
-# 
-# summary(pp_mod2)
-# 
-# pp_mod <- glm(pmin(round(ClaimAmount,0),5000) ~ Area +
-#                   VehPower +
-#                   VehBrand +
-#                   VehGas +
-#                   Region +
-#                   DrivAgeBand +
-#                   DensityBand + 
-#                   VehAgeBand + offset(log(Exposure)), data = train, family = poisson(link="log"))
-# summary(pp_mod)
-# 
-# plot(density(log(sev_train$severity)))
-# plot(density(log(sev_train$ClaimAmount)))
-# 
-# pp_pred <- predict(pp_mod, newdata = test, type="response")
-# pp_pred2 <- predict(pp_mod2, newdata=test, type="response")
-# 
-# rmse(test, "ClaimAmount",pp_pred) #19.94898
-# rmse(test, "ClaimAmount",pp_pred2) #19.95066
-# NRMSE(test, "ClaimAmount", pp_pred) #.000499707
-# NRMSE(test, "ClaimAmount", pp_pred2) #.000499749
-# 
+pp_mod2 <- earth(round(ClaimAmount,0) ~ Area +
+                   VehPower +
+                   VehBrand +
+                   VehGas +
+                   Region +
+                   DrivAgeBand +
+                   DensityBand +
+                   VehAgeBand + offset(log(Exposure)), data = train, degree = 2, glm=list(family = poisson(link="log")))
+
+summary(pp_mod2)
+
+
+pp_pred <- predict(pp_mod, newdata = test, type="response")
+
+pp_rmse <- rmse(test,"ClaimAmount", pp_pred)*100
+pp_nrmse <- 1-NRMSE(test,"ClaimAmount", pp_pred)*100
+pp_cor_pearson <- as.numeric(cor(pp_pred,test$ClaimAmount))
+pp_cor_spearman <- as.numeric(cor(pp_pred,test$ClaimAmount,method="spearman"))
+pp_mae <- MAE(pp_pred, test$ClaimAmount)
+pp_nmae <- 1-NMAE(pp_pred, test$ClaimAmount)*100
+pp_gini <- gini_value(pp_pred,test$Exposure)
+pp_agg_rpr <- agg_rpr(test, "ClaimAmount", pp_pred)
+pp_norm_rpd <- norm_rp_deviance(test, "ClaimAmount", pp_pred)
+
+rmse_compiled <- c(FS_rmse, pp_rmse)
+nrmse_compiled <- c(FS_nrmse, pp_nrmse)
+nmae_compiled <- c(FS_nmae, pp_nmae)
+mae_compiled <- c(FS_mae, pp_mae)
+pearson_cor_compiled <- c(FS_cor_pearson, pp_cor_pearson)
+spearman_cor_compiled <- c(FS_cor_spearman, pp_cor_spearman)
+gini_index_compiled <- c(FS_gini, pp_gini)
+agg_rpr_compiled <- c(FS_agg_rpr, pp_agg_rpr)
+norm_rpd_compiled <- c(FS_norm_rpd, pp_norm_rpd)
+
+model_metrics <- data.frame(c("MARS Freq/Sev","MARS PP"), rmse_compiled, mae_compiled, pearson_cor_compiled, spearman_cor_compiled, gini_index_compiled, agg_rpr_compiled)
+colnames(model_metrics) <- c("Model", "RMSE", "MAE", "Pearson (Linear) Correlation", "Spearman Rank Correlation", "Gini Index", "Aggregate Risk Premium Differential")
+eval_metrics <- data.frame(gini_index_compiled, nrmse_compiled, nmae_compiled, spearman_cor_compiled, norm_rpd_compiled)
+lscore <- apply(eval_metrics, 1, mean)
+gscore <- apply(eval_metrics, 1, geometric_mean)
+hscore <- apply(eval_metrics, 1, harmonic_mean)
+
+model_metrics$lscore <- lscore
+model_metrics$gscore <- gscore
+model_metrics$hscore <- hscore
+model_metrics[is.na(model_metrics)] <- 0
+
+write.csv(model_metrics,"final/Output/MARS/MARS_model_evaluation_stats.csv")
