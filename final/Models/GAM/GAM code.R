edb3476a06e8d9acdfd0d28eed88e16f4a07f5b9
TenTypesOfPeople <harshkarthikt@gmail.com>
2022-08-30 08:48:51 +0400

    New GAM code

--- final/Models/GAM/GAM code.R
+++ final/Models/GAM/GAM code.R
@@ -5,173 +5,96 @@ for(pkg in c("Metrics","dplyr","tidyr","gam","mgcv","mda","ggplot2","magrittr","
   }
 }
 
-#Libraries for evaluation functions
-library(Metrics)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(magrittr)
-library(pracma)
-
-#Libraries for models
-library(gam)
+#Load up libraries
 library(mgcv)
+library(Metrics)
+#library()
 
-#Data
+#source utils
+source("https://raw.githubusercontent.com/mlwp3/pricing/master/final/Utils/utils.R")
 
-fre.train<-read.csv(file.choose(),stringsAsFactors = FALSE) #final/train.csv from github.com/MLWP3/Pricing
-fre.test<-read.csv(file.choose(),stringsAsFactors = FALSE) #final/test.csv from github.com/MLWP3/Pricing
-fre.train$DrivAge<-as.numeric(as.character(fre.train$DrivAge)) #Just to be sure
-fre.test$DrivAge<-as.numeric(as.character(fre.test$DrivAge))
+train<-read.csv("https://github.com/mlwp3/pricing/blob/master/final/Data/train.csv?raw=true",stringsAsFactors = FALSE)
+test<-read.csv("https://github.com/mlwp3/pricing/blob/master/final/Data/test.csv?raw=true",stringsAsFactors = FALSE)
 
-#Evaluation functions
+#Set up models
 
-#Normalized RMSE
-NRMSE <- function(pred, obs) {
-  rmse(pred, obs)/(max(obs)-min(obs))
-}
+fre.gam.freq<-gam(ClaimNb ~
+                    Area +
+                    VehPower +
+                    s(VehAge,k=9) +
+                    s(DrivAge,k=9) +
+                    VehBrand +
+                    VehGas +
+                    s(Density,k=9) +
+                    Region +
+                    BonusMalus,
+                  family = poisson(link="log"),
+                  offset = log(Exposure),
+                  data = train,
+                  weights = rep(1,nrow(train))
+                )
 
-#For aggregated risk premium diff
-agg_rpr <- function(data, targetvar, prediction.obj) {
-  tot_obs <- sum(as.numeric(data[[targetvar]]))
-  tot_pred <- sum(as.numeric(prediction.obj))
-  rpr <- tot_obs / tot_pred
-  return(rpr)
-}
+fre.gam.sev<-gam(severity ~
+                    Area +
+                    VehPower +
+                    s(VehAge,k=9) +
+                    s(DrivAge,k=9) +
+                    VehBrand +
+                    VehGas +
+                    BonusMalus +
+                    s(Density,k=9) + 
+                    Region,
+                 data = train[train$severity>0,],
+                 family=Gamma(link="log"),
+                 weights = ClaimNb
+)
 
-#Gini
-gini_value <- function(predicted_loss_cost, exposure) {
-  lc_var <- enquo(predicted_loss_cost)
-  exp <- enquo(exposure)
-  dataset <- tibble(lc_var = !! lc_var, exp = !! exp)
-  dataset %>% 
-    arrange(lc_var) %>% 
-    mutate(cum_exp = cumsum(exp) / sum(exp),
-           cum_pred_lc = cumsum(lc_var) / sum(lc_var)) %$% 
-    {trapz(cum_exp, cum_pred_lc) %>% add(-1) %>% abs() %>% subtract(.5) %>% multiply_by(2)}
-}
+fre.gam.lc<-gam(ClaimAmount ~
+                  Area +
+                  VehPower +
+                  VehBrand +
+                  VehGas +
+                  s(Density) +
+                  Region +
+                  BonusMalus +
+                  s(DrivAge) +
+                  s(VehAge),
+                offset = log(Exposure),
+                data = train
+)
 
+
+#Predict outcomes
+
+pred.gam.freq <- predict(fre.gam.freq, newdata = test, type = "response")
+pred.gam.sev <- predict(fre.gam.sev, newdata = test, type = "response")
+pred.gam.lc <- predict(fre.gam.lc, newdata = test, type = "response")
+
+
+test$GAM<-pred.gam.freq*pred.gam.sev
+test$GAM_LC<-pred.gam.lc
+rmse(data=test,targetvar = "ClaimAmount",prediction.obj = test$GAM)
+rmse(data=test,targetvar = "ClaimAmount",prediction.obj = test$GAM_LC)
+
+#evaluate results
+
+#loss cost
+
+#Freq sev
+#RMSE
+test %$% RMSE(predicted_loss_cost_FS, observed_loss_cost)
+#NRMSE
+test %$% NRMSE(predicted_loss_cost_FS, observed_loss_cost)
+#MAE
+test %$% MAE(predicted_loss_cost_FS, observed_loss_cost)
+#Pearson correlation
+test %$% cor(predicted_loss_cost_FS,observed_loss_cost,method="pearson")
+#Spearman correlation
+test %$% cor(predicted_loss_cost_FS,observed_loss_cost,method="spearman")
+#Gini index
+test %$% gini_value(observed_loss_cost,predicted_loss_cost_FS, Exposure)
+#Aggregate RP differential
+test %$% agg_rpr(observed_loss_cost,predicted_loss_cost_FS)
 #Lift Curve
-lift_curve_plot <- function(predicted_loss_cost, observed_loss_cost, exposure, n) {
-  pred_lc <- enquo(predicted_loss_cost)
-  obs_lc <- enquo(observed_loss_cost)
-  exp <- enquo(exposure)
-  dataset <- tibble(pred_lc = !! pred_lc, obs_lc = !! obs_lc, exp = !! exp)
-  dataset <- dataset %>% 
-    arrange(exp) %>% 
-    mutate(buckets = ntile(exp, n)) %>% 
-    group_by(buckets) %>% 
-    summarise(Predicted_Risk_Premium = mean(pred_lc),
-              Observed_Risk_Premium = mean(obs_lc), 
-              Exposure = sum(exp))
-  max_bucket <- which.max(dataset$Exposure)
-  base_predicted_rp <- dataset[max_bucket, ]$Predicted_Risk_Premium
-  base_observed_rp <- dataset[max_bucket, ]$Observed_Risk_Premium
-  dataset <- dataset %>%
-    select(-Exposure) %>%
-    mutate(Predicted_Risk_Premium = Predicted_Risk_Premium / base_predicted_rp, 
-           Observed_Risk_Premium = Observed_Risk_Premium / base_observed_rp)  %>%
-    tidyr::pivot_longer(c(Predicted_Risk_Premium, Observed_Risk_Premium)) %>%
-    ggplot() +
-    geom_line(aes(x = as.factor(buckets), y = value, col = name, group = name)) +
-    geom_point(aes(x = as.factor(buckets), y = value, col = name, group = name)) +
-    xlab("Bucket") + ylab("Average Risk Premium")
-}
 
-#Double Lift Curve
-double_lift_chart <- function(predicted_loss_cost_mod_1, predicted_loss_cost_mod_2, observed_loss_cost, exposure, n) {
-  pred_lc_m1 <- enquo(predicted_loss_cost_mod_1)
-  pred_lc_m2 <- enquo(predicted_loss_cost_mod_2)
-  obs_lc <- enquo(observed_loss_cost)
-  exp <- enquo(exposure)
-  dataset <- tibble(pred_lc_m1 = !! pred_lc_m1, pred_lc_m2 = !! pred_lc_m2, obs_lc = !! obs_lc, exp = !! exp)
-  dataset <- dataset %>%
-    mutate(sort_ratio = pred_lc_m1 / pred_lc_m2) %>%
-    arrange(exp) %>% 
-    mutate(buckets = ntile(exp, n)) %>% 
-    group_by(buckets) %>% 
-    summarise(Model_1_Predicted_Risk_Premium = mean(pred_lc_m1),
-              Model_2_Predicted_Risk_Premium = mean(pred_lc_m2),
-              Observed_Risk_Premium = mean(obs_lc), 
-              Exposure = sum(exp))
-  max_bucket <- which.max(dataset$Exposure)
-  base_predicted_rp1 <- dataset[max_bucket, ]$Model_1_Predicted_Risk_Premium
-  base_predicted_rp2 <- dataset[max_bucket, ]$Model_2_Predicted_Risk_Premium
-  base_observed_rp <- dataset[max_bucket, ]$Observed_Risk_Premium 
-  dataset <- dataset %>%
-    select(-Exposure) %>%
-    tidyr::pivot_longer(c(Model_1_Predicted_Risk_Premium, Model_2_Predicted_Risk_Premium, Observed_Risk_Premium)) %>%
-    ggplot() +
-    geom_line(aes(x = as.factor(buckets), y = value, col = name, group = name)) +
-    geom_point(aes(x = as.factor(buckets), y = value, col = name, group = name)) +
-    xlab("Bucket") + ylab("Average Risk Premium")
-}
-
-#Gini plot
-gini_plot <- function(predicted_loss_cost, exposure){
-  
-  lc_var <- enquo(predicted_loss_cost)
-  exp <- enquo(exposure)
-  
-  dataset <- tibble(lc_var = !! lc_var, exp = !! exp)
-  
-  dataset %>% 
-    arrange(lc_var) %>% 
-    mutate(pred_losses = lc_var * exp,
-           cum_exp = cumsum(exp) / sum(exp),
-           cum_pred_lc = cumsum(pred_losses) / sum(pred_losses)) %>% 
-    ggplot() +
-    geom_line(aes(x = cum_exp, y = cum_pred_lc)) +
-    geom_abline(intercept = 0, slope = 1) +
-    xlab("Percentage of Exposure") +
-    ylab("Percentage of Losses")
-}
-
-
-#Models
-
-predictors<-c("bs(DrivAge)","bs(BonusMalus)","VehPower","Area","VehGas","VehBrand","bs(Density)","Region")
-
-#Pure premium
-
-#MGCV - GAM
-fre.GAM.2<-mgcv::gam(as.formula(paste("ClaimAmount~",paste(predictors,collapse="+"),sep="")),data=fre.train)
-pred.fre.GAM.2<-predict(fre.GAM.2,newdata=fre.test)
-
-#F/S
-
-#MGCV - GAM
-fre.GAM.2.freq<-mgcv::gam(as.formula(paste("ClaimNb~",paste(predictors,collapse="+"),sep="")),data=fre.train)
-fre.GAM.2.sev<-mgcv::gam(as.formula(paste("severity~",paste(predictors,collapse="+"),sep="")),data=fre.train, subset = ClaimNb > 0, weights = ClaimNb)
-pred.fre.GAM.2.freq<-predict(fre.GAM.2.freq,newdata=fre.test)
-pred.fre.GAM.2.sev<-predict(fre.GAM.2.sev,newdata=fre.test)
-
-#Results
-
-#Pure premium
-
-#MGCV - GAM
-rmse(pred.fre.GAM.2,fre.test$ClaimAmount)                               #RMSE
-mae(predicted = pred.fre.GAM.2, actual = fre.test$ClaimAmount)          #MAE
-cor(pred.fre.GAM.2,fre.test$ClaimAmount)                                #Pearson correlation
-cor(pred.fre.GAM.2,fre.test$ClaimAmount,method="spearman")              #Spearman correlation
-gini_value(pred.fre.GAM.2, fre.test$Exposure)  #Gini index
-agg_rpr(fre.test, "ClaimAmount", pred.fre.GAM.2)                        #Aggregated risk prem diff
-#NRMSE(pred=pred.fre.GAM.2,obs=fre.test$ClaimAmount)
-gini_plot(pred.fre.GAM.2,fre.test$Exposure)
-gini_value(pred.fre.GAM.2,fre.test$Exposure)
-lift_curve_plot(pred.fre.GAM.2,fre.test$ClaimAmount,fre.test$Exposure,25)
-
-#F/S
-
-#MGCV - GAM
-rmse(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev,fre.test$ClaimAmount)                               #RMSE
-mae(predicted = pred.fre.GAM.2.freq*pred.fre.GAM.2.sev, actual = fre.test$ClaimAmount)          #MAE
-cor(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev,fre.test$ClaimAmount)                                #Pearson correlation
-cor(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev,fre.test$ClaimAmount,method="spearman")              #Spearman correlation
-gini_value(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev, fre.test$Exposure)  #Gini index
-agg_rpr(fre.test, "ClaimAmount", pred.fre.GAM.2.freq*pred.fre.GAM.2.sev)                        #Aggregated risk prem diff
-#NRMSE(pred=pred.fre.GAM.2,obs=fre.test$ClaimAmount)
-gini_plot(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev,fre.test$Exposure)
-gini_value(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev,fre.test$Exposure)
-lift_curve_plot(pred.fre.GAM.2.freq*pred.fre.GAM.2.sev,fre.test$ClaimAmount,fre.test$Exposure,25)
+#Double Lift Chart
